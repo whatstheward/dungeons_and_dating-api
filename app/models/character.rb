004d5b8dde4328b85ee839ca52d24916c339da62
github username <github email address>
2020-04-28 14:21:14 -0400

    add routes

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,5 @@ class Character < ApplicationRecord
     belongs_to :user
     has_many :interests, foreign_key: :interest_id, class_name: 'Relationship'
     has_many :suitors, through: :interests
+
 end
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -2,4 +2,6 @@ class Stat < ApplicationRecord
   belongs_to :character, optional:true
 
   validates :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 20}
+
+  
 end

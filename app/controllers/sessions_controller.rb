66f2936fdd13713e3b1224e3f7f9526cfcf2b37e
github username <github email address>
2020-04-30 16:24:45 -0400

    send token with signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
     skip_before_action :authorize!, only: [:create]
-  def create
+  
+    def create
     @user = User.find_by(email: params[:email])
     if @user && @user.password = params[:password]
       payload = {user_id: @user.id}
       token = JWT.encode payload, ENV["ENV_SECRET"], 'HS256'
       render json: {token: token}, status: 200
     else
-      render json: {errors:["Invalid login credentials"]}, status: unauthorized
+      render json: {errors:["Invalid login credentials"]}
     end
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
         user_json = UserSerializer.new(@user).serialized_json
         render json: user_json
     end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            payload = {user_id: @user.id}
+            token = JWT.encode payload, ENV["ENV_SECRET"], 'HS256'
+            render json: {token: token}, status: 200
+        else
+            render json: @user.errors.full_messages
+        end
+    end
+
+    private
+    
+    def user_params
+        params.permit(:first_name, :last_name, :email, :password)
+    end
 end
